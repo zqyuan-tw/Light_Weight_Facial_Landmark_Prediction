2c07b405fb3ea47f6feb967fe9af22c507a4ad41
Yuan <r10921061@ntu.edu.tw>
2022-06-01 21:56:55 +0800

    minor change

--- face.py
+++ face.py
@@ -5,13 +5,17 @@ import os
 import pickle
 
 class FaceDataset(Dataset):
-    def __init__(self, prefix, transform) -> None:
+    def __init__(self, prefix, transform, do_train=True) -> None:
         super().__init__()
         self.prefix = prefix
         self.transform = transform
-        with open(os.path.join(self.prefix, 'annot.pkl'), 'rb') as f:
-            self.imgs, self.landmarks = pickle.load(f)
-        self.landmarks = np.array(self.landmarks)
+        self.landmarks = None
+        if do_train:
+            with open(os.path.join(self.prefix, 'annot.pkl'), 'rb') as f:
+                self.imgs, self.landmarks = pickle.load(f)
+            self.landmarks = np.array(self.landmarks)
+        else:
+            self.imgs = os.listdir(self.prefix)
 
     def __len__(self):
         return len(self.imgs)
@@ -19,5 +23,8 @@ class FaceDataset(Dataset):
     def __getitem__(self, index):
         img = self.transform(Image.open(os.path.join(self.prefix, self.imgs[index])))
         C, H, W = img.shape
-        norm_landmarks = np.stack((self.landmarks[index, :, 0] / W, self.landmarks[index, :, 1] / H), axis=1)
-        return img, norm_landmarks
+        if self.landmarks is not None:
+            norm_landmarks = np.stack((self.landmarks[index, :, 0] / W, self.landmarks[index, :, 1] / H), axis=1)
+            return img, norm_landmarks
+        else:
+            return img, 0
--- main.py
+++ main.py
@@ -29,6 +29,9 @@ def get_model(model):
 
 if __name__ == "__main__":
     args = parse_args()
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
     if args.do_train is not None:
         with open(args.do_train) as f:
             config = json.load(f)
@@ -47,7 +50,6 @@ if __name__ == "__main__":
         else:
             val_dataset = None
             val_epoch = 0
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
         scheduler = None

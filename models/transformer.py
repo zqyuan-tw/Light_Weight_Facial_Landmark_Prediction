3bd9e6ef37b12bd02513d80563534cf6fccfdb05
Yuan <r10921061@ntu.edu.tw>
2022-06-14 10:03:18 +0800

    enlarge vit

--- models/transformer.py
+++ models/transformer.py
@@ -7,13 +7,14 @@ class Transformer(nn.Module):
         super(Transformer, self).__init__()
         self.vit = MobileViT(
             image_size = (384, 384),
-            dims = [96, 120, 144],
-            channels = [16, 32, 48, 48, 64, 64, 80, 80, 96, 96, 384],
+            dims = [120, 144, 168],
+            channels = [16, 32, 64, 64, 96, 96, 128, 128, 160, 160, 640],
             num_classes = 136
-        )            
+        )
+        self.tanh = nn.Tanh()            
 
     def forward(self, x):
-        x = self.vit(x)
+        x = self.tanh(self.vit(x))
 
         return x.view(x.size(0), 68, 2)
 
